3519900e0c5b495827f801fad212cd1b1354a041
Mvye <mervyn.mathew18@gmail.com>
2021-02-28 23:17:16 +0000

    Modified app.py for better readability

--- app.py
+++ app.py
@@ -29,7 +29,17 @@ def on_connect():
 def on_disconnect():
     print('User disconnected!')
 
-def createPlayerData(players, spectators, status, username):
+players = []
+spectators = []
+def updateStatus(username):
+    if (len(players) < 2):
+        players.append(username)
+        return players.index(username)
+    else:
+        spectators.append(username)
+        return 2
+
+def createPlayerData(status, username):
     data = {
         "players": players,
         "spectators": spectators,
@@ -37,33 +47,22 @@ def createPlayerData(players, spectators, status, username):
         "username": username
     }
     return data
-    
-players = []
-spectators = []
+
 @socketio.on('requestLogin')
 def on_request_login(data):
     username = data["requestedUsername"]
     sid = data["sid"]
-    if (len(players) < 2):
-        players.append(username)
-        status = players.index(username)
-    else:
-        spectators.append(username)
-        status = 2
     new_data = {
         "players": players,
         "spectators": spectators
     }
+    status = updateStatus(username)
     socketio.emit('joined', new_data, broadcast=True, include_self=False)
-    socketio.emit('approved', createPlayerData(players, spectators, status, username), room=sid)
+    socketio.emit('approved', createPlayerData(status, username), room=sid)
 
-# When a client emits the event 'chat' to the server, this function is run
-# 'chat' is a custom event name that we just decided
 @socketio.on('turn')
-def on_turn(data): # data is whatever arg you pass in your emit call on client
+def on_turn(data):
     print(str(data))
-    # This emits the 'chat' event from the server to all clients except for
-    # the client that emmitted the event that triggered this function
     socketio.emit('turn',  data, broadcast=True, include_self=False)
     socketio.emit('switch', data, broadcast=True, include_self=True)
 
@@ -75,17 +74,27 @@ def on_end(data):
     socketio.emit('end',  data, broadcast=True, include_self=False)
     socketio.emit('voting', votes, broadcast=True, include_self=True)
 
+def canVote(username):
+    if username in players and username not in voted:
+        return True
+    else:
+        return False
+
+def addVote(username):
+    print("Received vote from " + username)
+    voted.append(username)
+    return { "vote": len(voted) }
+
 @socketio.on('vote')
 def on_vote(data):
     username = data["username"]
-    if username in players and username not in voted:
-        print("Received vote from " + username)
-        voted.append(username)
-        votes = { "vote": len(voted) }
-        socketio.emit('voting', votes, broadcast=True, include_self=True)
-        if len(voted) == 2:
-            socketio.emit('again', votes, broadcast=True, include_self=True)
-            voted.clear()
+    if canVote == False:
+        print("Invalid vote received")
+    votes = addVote(username)
+    socketio.emit('voting', votes, broadcast=True, include_self=True)
+    if len(voted) == 2:
+        socketio.emit('again', votes, broadcast=True, include_self=True)
+        voted.clear()
         
 
 # Note that we don't call app.run anymore. We call socketio.run with app arg
